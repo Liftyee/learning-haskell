822827d517a6b6b12edbb22774bc3b5dd1a21c33
Victor Liu <victor.yuchen.liu@gmail.com>
2023-09-12 11:15:35 +0100

    ex13

--- ex10.hs
+++ ex10.hs
@@ -1,7 +1,4 @@
-partRoot:: Float -> Float -> Float -> Float
-partRoot a b c = sqrt(b * b - 4 * a * c)
-
 quadraticRoots:: Float -> Float -> Float -> (Float, Float)
-quadraticRoots a b c = ((-b +partRoot a b c)/(2 * a), 
-                        (-b -partRoot a b c)/(2 * a))
+quadraticRoots a b c =  let p = sqrt(b * b - 4 * a * c)
+    in ((-b + p)/(2 * a), (-b - p)/(2 * a))
 main = print (quadraticRoots 5 11 (-12))
